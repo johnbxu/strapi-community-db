f097c98f637c5796f391dac9afe6f0ac2bcae8e7
johnbxu <johnbxu@gmail.com>
2021-04-05 14:26:16 -0400

    Add packages

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aa3b8c439d5694c10d9bdedccbb52762'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9d846197-00d9-448f-8df8-ab443ae1c04e'
+};
